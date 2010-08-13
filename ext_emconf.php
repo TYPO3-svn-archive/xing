432e2345c80b5ae4a83be0dbbce013ebcda3d3f4
peter@eifel <peter@eifel@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-08-13 18:29:29 +0000

    Initial commit
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/xing/trunk@36741 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "xing".
+#
+# Auto generated 13-08-2010 20:25
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'XING Widgets',
+	'description' => 'The XING widgets for your TYPO3 site',
+	'category' => 'plugin',
+	'author' => 'Peter Proell',
+	'author_email' => 'peter@alinbu.net',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"7a7c";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"707b";s:14:"ext_tables.php";s:4:"290c";s:13:"locallang.xml";s:4:"37fa";s:16:"locallang_db.xml";s:4:"ebea";s:19:"doc/wizard_form.dat";s:4:"4fda";s:20:"doc/wizard_form.html";s:4:"77ba";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:25:"pi1/class.tx_xing_pi1.php";s:4:"833a";s:33:"pi1/class.tx_xing_pi1_wizicon.php";s:4:"f1aa";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"00c6";s:33:"static/xing_widgets/constants.txt";s:4:"d41d";s:29:"static/xing_widgets/setup.txt";s:4:"d41d";}',
+);
+
+?>
